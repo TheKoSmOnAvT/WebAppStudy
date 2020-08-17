a25411df975017b6830bd255f10866b1b7163edf
TheKoSmOnAvT <ni_popov@mail.ru>
2020-08-17 12:56:22 +0500

    Initial Commit

---
+++ webStudyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  webStudyApp
+//
+//  Created by Никита Попов on 17.08.2020.
+//  Copyright © 2020 Никита Попов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
